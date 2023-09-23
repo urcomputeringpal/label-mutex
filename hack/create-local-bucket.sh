7d21dc388e07234dc2fec94f090f48185ad757bd
Jesse Newland <jnewland@urcomputeringpal.com>
2023-09-23 09:38:59 -0500

    don't understand why these dns names don't work

--- hack/create-local-bucket.sh
+++ hack/create-local-bucket.sh
@@ -2,8 +2,9 @@
 
 bucket_name=label-mutex
 project_id=fake-project
+endpoint=${1:-fake-gcs-server}
 
-gsutil -o "Credentials:gs_json_host=fake-gcs-server" -o "Credentials:gs_json_port=4443" -o "Boto:https_validate_certificates=False" mb -p "${project_id}" "gs://${bucket_name}"
+gsutil -o "Credentials:gs_json_host=$endpoint" -o "Credentials:gs_json_port=4443" -o "Boto:https_validate_certificates=False" mb -p "${project_id}" "gs://${bucket_name}"
 
 # apparently implied with fake-gcs-server
 # gsutil -o "Credentials:gs_json_host=fake-gcs-server" -o "Credentials:gs_json_port=4443" -o "Boto:https_validate_certificates=False" versioning set on  -p "${project_id}" "gs://${bucket_name}"
